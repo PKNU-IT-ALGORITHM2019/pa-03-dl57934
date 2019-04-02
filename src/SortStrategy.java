c1fac6c54efd720481e405bcb1244389ad174e4d
dl57934 <dl57934@pukyong.ac.kr>
2019-04-02 20:55:00 +0900

    change photo

--- src/SortStrategy.java
+++ src/SortStrategy.java
@@ -1,41 +1,41 @@
 public abstract class SortStrategy {
     final static int RANDOM = 0, REVERSE = 1;
-    private  OccurData occurData = new OccurData();
+    private OccurData occurData = new OccurData();
     private int type = 0, size = 1000;
+
     abstract void sort();
+
     abstract void swap(int a, int b);
+
     abstract public String getName();
 
-
-    public int getType(){
+    public int getType() {
         return type;
     }
 
-    public int getSize(){
+    public int getSize() {
         return size;
     }
 
-    public void setSize(int size){
+    public void setSize(int size) {
         this.size = size;
     }
 
-    public void setType(int type){
+    public void setType(int type) {
         this.type = type;
     }
 
-
-
-    public String execute(){
+    public String execute() {
         long start = System.currentTimeMillis();
         sort();
         long end = System.currentTimeMillis();
-        return Long.toString(end-start);
+        return Long.toString(end - start);
     }
 
-    public int[] setArray(int size, int type, int[] arr){
-        if(SortStrategy.RANDOM == type)
+    public int[] setArray(int size, int type, int[] arr) {
+        if (SortStrategy.RANDOM == type)
             occurData.RandomInitData(size);
-        else if(SortStrategy.REVERSE == type)
+        else if (SortStrategy.REVERSE == type)
             occurData.ReverseInitDat(size);
         arr = occurData.getter();
         return arr;
